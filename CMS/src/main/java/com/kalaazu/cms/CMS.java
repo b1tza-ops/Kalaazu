95349794cc932bf2b965d063eb17a874452c2b0c
Paul Scripcariu <paul1mantech@gmail.com>
2026-08-29 22:56:13 +0100

    feat: configure local HTML5 client

--- CMS/src/main/java/com/kalaazu/cms/CMS.java
+++ CMS/src/main/java/com/kalaazu/cms/CMS.java
@@ -3,6 +3,8 @@ package com.kalaazu.cms;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -23,5 +25,17 @@ public class CMS implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // disable CORS
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:/index.html");
     }
 }
